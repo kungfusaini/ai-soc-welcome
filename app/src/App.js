6655493a233f084487b7711d1ec335642a221ab9
Sumeet Saini <sumeetsaini621@gmail.com>
2022-09-15 14:31:46 +0100

    creates form which accepts values

--- app/src/App.js
+++ app/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
       <div>
         <DisplayBanner />
+        <NameForm />
       </div>
       
 
@@ -15,6 +16,49 @@ function App() {
 
 function DisplayBanner(){
   return <img src={banner} alt="banner" class="center"/>
+}
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first:'',
+      last:'',
+      email:''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.email);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label for="fname">First Name</label>
+        <input type="text" name="first" value={this.state.first} onChange={this.handleChange}/>
+        <label for="lname">Last Name</label>
+        <input type="text" name="last" value={this.state.last} onChange={this.handleChange}/>
+        <label for="email">Email</label>
+        <input type="text" name="email" value={this.state.email} onChange={this.handleChange}/>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 export default App;
