50ab1cdf03e7222c514e3f150b2cfca4d4ed5644
Sumeet Saini <sumeetsaini621@gmail.com>
2022-09-22 18:14:56 +0100

    adds image gallery for generated images

--- app/src/App.js
+++ app/src/App.js
@@ -2,28 +2,71 @@ import banner from './static/banner.png';
 import './App.css';
 import React from 'react';
 import Popup from './components/Popup';
-import { useState } from 'react';
+import { useState, setState } from 'react';
+import ImageView from './components/ImageView';
+
+
 
 function App() {
+  const [img, setImages] = useState([]);
+  console.log("hey");
+  generateImageTiles(img, setImages);
   const [buttonPopup, setButtonPopup] = useState(false);
   return (
       <div>
         <DisplayBanner />
         <button onClick={()=>setButtonPopup(true)}openPopup>Open Popup</button>
+        <ImageView images={img}/>
         <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
         </Popup>
       </div>
+      
       
 
   );
 }
 
 function DisplayBanner(){
-  return <img src={banner} alt="banner" class="center"/>
+  return <img src={banner} alt="banner" className="center"/>
 }
 
 
 
+async function generateImageTiles(img, updateImages){
+  let response = await fetch('http://127.0.0.1:5000/getArt', {
+  method: 'GET',
+  mode: 'cors',
+  headers: {
+    'Content-Type': 'application/json'
+    }
+  });
+  const art = await response.json();
+  const images = [];
+
+  for (var key in art) {
+    images.push({
+      src: art[key],
+      width: 1024,
+      height: 1024,
+      customOverlay: (
+        <div className="custom-overlay__caption">
+          <div>{key}</div>
+        </div>
+      )
+    })
+  }
+  
+// setTimeout(generateImageTiles, 1000, updateImages);
+if (img.length != images.length ) {
+  updateImages(images);
+}
+else{
+  setTimeout(generateImageTiles, 1000, img, updateImages);
+}
+}
+
+  
+ 
 
 
 
---
+++ app/src/components/ImageView.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Gallery} from 'react-grid-gallery';
+import './ImageView.css';
+
+
+function ImageView(props){
+ return <Gallery images={props.images}/>;
+}
+
+export default ImageView
+
+
+
